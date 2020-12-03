1298ae2968ad99e617e8601fc74393cc67cff6e2
ParadiseFallen <50731756+ParadiseFallen@users.noreply.github.com>
2020-12-03 19:01:41 +0200

    Done ApiClient base project

---
+++ APIClient/RestApiServiceBase.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ApiClient
+{
+    public abstract class RestApiServiceBase
+    {
+        /// <summary>
+        /// DO NOT CALL <see cref="RestClient.GetAsync()"/>, <see cref="RestClient.PostAsync()"/>, <see cref="RestClient.PutAsync()"/> etc
+        /// DIRECTLY. Use protected methods
+        /// </summary>
+        public RestClient RestClient { get; }
+        public JsonSerializerOptions SerializerOptions { get; set; }
+
+        public RestApiServiceBase(RestClient restClient)
+        {
+            RestClient = restClient ?? throw new NullReferenceException($"APIClient is null");
+        }
+
+        protected async Task<HttpResponseMessage> GetAsync(string uri) => 
+            await RestClient.GetAsync(uri);
+
+        protected async Task<HttpResponseMessage> PostAsync<T>(string uri, T data) => 
+            await RestClient.PostAsJsonAsync(uri,data, SerializerOptions);
+
+        protected async Task<HttpResponseMessage> PostAsync(string uri) =>
+            await RestClient.PostAsync(uri,null);
+
+        protected async Task<HttpResponseMessage> PutAsync<T>(string uri, T data)=>
+            await RestClient.PutAsJsonAsync(uri, data, SerializerOptions);
+
+        protected async Task<HttpResponseMessage> DeleteAsync<T>(string uri)=>
+            await RestClient.DeleteAsync(uri);
+
+        /// <summary>
+        /// Safe execution of request. Wrapped by <c>Try Catch</c>.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="requestMethod">Task with request</param>
+        /// <param name="reciveHandler">Handler for recived HttpResponseMessage. Read <see cref="T"/> from <see cref="HttpResponseMessage.Content"/> <c>response.Content.ReadFromJsonAsync<T>(SerializerOptions)</c> </param>
+        /// <param name="exceptionHandler">Handler for exception. Suppresses by default and return null</param>
+        /// <returns><see cref="T"/></returns>
+        protected virtual async Task<T> ExecuteRequestAsync<T>(
+            Task<HttpResponseMessage> requestMethod,
+            Func<HttpResponseMessage, Task<T>> reciveHandler = null,
+            Func<Exception, Task<T>> exceptionHandler = null)
+        {
+            reciveHandler ??= new Func<HttpResponseMessage, Task<T>>(
+                response => 
+                    response.Content.ReadFromJsonAsync<T>(SerializerOptions));
+            exceptionHandler ??= new Func<Exception, Task<T>>(exception => null);
+
+            try
+            {
+                return await reciveHandler(await requestMethod);
+            }
+            catch (Exception ex)
+            {
+                return await exceptionHandler(ex);
+            }
+        }
+    }
+}
---
+++ APIClient/RestClientFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiClient
+{
+    public class RestClientFactory
+    {
+
+        public RestClientFactory()
+        {
+
+        }
+
+        public RestClient Create()
+        {
+            var handler = new HttpClientHandler();
+            return new RestClient(handler);
+        }
+
+    }
+}
