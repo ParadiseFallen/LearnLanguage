eac824a789713d69567b4284951e8b083f1fe466
ParadiseFallen <50731756+ParadiseFallen@users.noreply.github.com>
2020-12-05 12:13:26 +0200

    not working extension

---
+++ ApiServices/Extensions/RestClientExtensions.cs
@@ -0,0 +1,17 @@
+﻿using ApiClient;
+using System;
+using System.Net;
+
+namespace ApiServices.Extensions
+{
+    public static class RestClientExtensions
+    {
+        public static string AuthCookieName { get; set; } = ".AspNetCore.Identity.Application";
+
+        public static void SetAuthCookie(this RestClient client, string data) =>
+            client.Handler.CookieContainer.Add(new Cookie(AuthCookieName, data, "/"));
+
+        public static string GetAuthCookie(this RestClient client) =>
+            client.Handler.CookieContainer.GetCookies(new Uri("/"))[AuthCookieName].Value;
+    }
+}
