05027512eba57f79bc84dbfbcf3694083c047713
ParadiseFallen <50731756+ParadiseFallen@users.noreply.github.com>
2020-12-05 12:13:11 +0200

    impl services

--- ApiServices/Services/AccountService.cs
+++ ApiServices/Services/AccountService.cs
@@ -2,6 +2,11 @@
 using ApiServices.Endpoints;
 using ApiServices.ServicesInterfaces;
 using Models;
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace ApiServices.Services
@@ -10,15 +15,40 @@ namespace ApiServices.Services
     {
         public AccountService(RestClient restClient) : base(restClient) { }
 
-        public Task<ApiResponse<User>> Login(Login login) =>
-            ExecuteRequestAsync<ApiResponse<User>>(PostAsync(Api.Account.Login,login));
-        public Task<ApiResponse<bool>> Logout() =>
-            ExecuteRequestAsync(
-                PostAsync(Api.Account.Logout),
-                async respone=>new ApiResponse<bool>(null,respone.IsSuccessStatusCode)
-                );
-        public Task<ApiResponse<User>> Register(Login user) =>
-            ExecuteRequestAsync<ApiResponse<User>>(PostAsync(Api.Account.Login, user));
 
+        private Func<Exception, Task<ApiResponse<UserInfo>>> ExceptionHandlerUserInfo =>
+            new Func<Exception, Task<ApiResponse<UserInfo>>>(async ex =>
+        {
+            if (ex is HttpRequestException)
+                return new ApiResponse<UserInfo>() { Errors = new[] { "Server not responding." } };
+            return new ApiResponse<UserInfo>() { Errors = new[] { "Unhandled exception!" } };
+        });
+
+        public Task<ApiResponse<UserInfo>> Login(Login login) =>
+            CreateRequest<ApiResponse<UserInfo>>(PostAsync(Api.Account.Login, login))
+            .SetExceptionHandler(ExceptionHandlerUserInfo)
+            .ExecuteRequestAsync();
+
+        public Task<ApiResponse<bool>> Logout() =>
+            CreateRequest<ApiResponse<bool>>(PostAsync(Api.Account.Logout))
+            .SetResponseHandler(async respone => new ApiResponse<bool>(null, respone.IsSuccessStatusCode))
+            .ExecuteRequestAsync();
+
+        public Task<ApiResponse<bool>> Register(Login user) =>
+            CreateRequest<ApiResponse<bool>>(PostAsync(Api.Account.Register, user))
+            .SetResponseHandler(
+                async response =>
+                {
+                    var apiResponse = await response.Content.ReadFromJsonAsync<ApiResponse<object>>(SerializerOptions);
+                    return new ApiResponse<bool>() { Content = response.IsSuccessStatusCode, Errors = apiResponse.Errors };
+                })
+            .SetExceptionHandler(
+                async ex =>
+                {
+                    if (ex is HttpRequestException)
+                        return new ApiResponse<bool> { Errors = new[] { "Server not responding." }, Content = false };
+                    return new ApiResponse<bool> { Errors = new[] { "Unhandled exception!" }, Content = false };
+                })
+            .ExecuteRequestAsync();
     }
 }
---
+++ ApiServices/Services/ApiService.cs
@@ -0,0 +1,22 @@
+﻿using ApiClient;
+using ApiServices.Endpoints;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiServices.Services
+{
+    public class ApiService : RestApiServiceBase
+    {
+        public ApiService(RestClient restClient) : base(restClient)
+        {
+        }
+
+        public async Task<bool> IsApiAvailable() => await ExecuteRequestAsync(
+            GetAsync(Api.ApiBase),
+            async x => x.IsSuccessStatusCode,
+            async ex => false);
+    }
+}
