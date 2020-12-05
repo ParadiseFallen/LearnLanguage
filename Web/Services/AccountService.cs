ca59dbf9c5bab6f706976f03f396f7be33df34a3
ParadiseFallen <50731756+ParadiseFallen@users.noreply.github.com>
2020-12-05 12:31:36 +0200

    Revert "using new ApiResponse<T>"
    This reverts commit bab19ecf9d3f9bf800e045b17a837a0ae54d5936.

--- Web/Services/AccountService.cs
+++ Web/Services/AccountService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Models;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -10,16 +11,26 @@ namespace Web.Services
     {
         private SignInManager<User> SigninManager { get; }
         private UserManager<User> UserManager { get; }
-        public AccountService(DatabaseContext db, SignInManager<User> signInManager, UserManager<User> userManager ) : base(db)
+        public AccountService(DatabaseContext db, SignInManager<User> signInManager, UserManager<User> userManager) : base(db)
         {
             SigninManager = signInManager;
             UserManager = userManager;
         }
 
-
-        public async Task<SignInResult> LoginAsync(Login login)
+        public async Task<ApiResponse<UserInfo>> LoginAsync(Login login)
         {
-            return await SigninManager.PasswordSignInAsync(login.Username, login.Password, false, false);
+            var result =  await SigninManager.PasswordSignInAsync(login.Username, login.Password, false, false);
+            if (result.Succeeded)
+            {
+                var user = await UserManager.FindByNameAsync(login.Username);
+                return new ApiResponse<UserInfo>() { Content = new UserInfo() 
+                {
+                    Username = user.UserName,
+                    Email=user.Email,
+                    NativeCulture = user.NativeCulture 
+                } }; //set user
+            }
+            return new ApiResponse<UserInfo>() { Errors = new[] { "Wrong login or password." } };
         }
         public async Task LogoutAsync()
         {
@@ -40,9 +51,5 @@ namespace Web.Services
 
             return createdUser.Errors;
         }
-        //public async Task<bool> UpdateUser()
-        //{
-
-        //}
     }
 }
